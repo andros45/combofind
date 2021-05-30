ec52ab5280778aacc50fd8af74b8d4213d5bd893
Andre Johansen <andjoh333@gmail.com>
2021-05-30 13:09:37 +0200

    Added .gitignore

--- readstring.c
+++ readstring.c
@@ -6,24 +6,18 @@
 int main(void)
 {
 
-    char str[100];
-    printf("Enter a string: ");
-    fgets(str, sizeof(str), stdin);
-    printf("The entered string: %s",str);
-
-/*
     int siz;
     char i[BUFLEN];
 
     printf("Enter a string.\n");
-    
+    fflush(stdout);
     fgets(i, BUFLEN, stdin);
 
     siz = strlen(i) - 1;
-    
+
     printf("siz is %d\n", siz);
     
     printf("hit <return> to exit program\n");
-    getc(stdin);*/
+    getc(stdin);
     return 0;
 }
